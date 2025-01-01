d36040d49d2eb66ee65476618e7775b510d0f3fe
Subhajit Mallick <153619690+subhajitxyz@users.noreply.github.com>
2025-01-01 08:35:23 +0530

    remove inject

--- app/src/main/java/org/oppia/android/app/devoptions/devoptionsitemviewmodel/DeveloperOptionsAddProfileViewModel.kt
+++ app/src/main/java/org/oppia/android/app/devoptions/devoptionsitemviewmodel/DeveloperOptionsAddProfileViewModel.kt
@@ -3,7 +3,6 @@ package org.oppia.android.app.devoptions.devoptionsitemviewmodel
 
 import javax.inject.Inject
 import org.oppia.android.app.devoptions.AddProfilesClickListener
-import org.oppia.android.app.profile.AddProfileActivityPresenter
 
 /**
  * [DeveloperOptionsItemViewModel] to provide features to test and debug math expressions and
@@ -12,8 +11,6 @@ import org.oppia.android.app.profile.AddProfileActivityPresenter
 class DeveloperOptionsAddProfileViewModel(
   private val addProfilesClickListener: AddProfilesClickListener
 ) : DeveloperOptionsItemViewModel() {
-  @Inject
-  lateinit var addProfileFragmentPresenter: AddProfileActivityPresenter
 
    fun addThreeProfiles() {
     addProfilesClickListener.createThreeProfiles()
@@ -24,6 +21,5 @@ class DeveloperOptionsAddProfileViewModel(
   fun deleteProfiles() {
     addProfilesClickListener.deleteProfiles()
   }
-
 
 }
