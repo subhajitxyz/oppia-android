cd3bd6d6677ed5d4e1444c170c952d8084b907cb
Subhajit Mallick <153619690+subhajitxyz@users.noreply.github.com>
2025-01-01 08:03:53 +0530

    try to correct devDeveloperOptionsAddProfileViewModel import

--- app/src/main/java/org/oppia/android/app/devoptions/DeveloperOptionsViewModel.kt
+++ app/src/main/java/org/oppia/android/app/devoptions/DeveloperOptionsViewModel.kt
@@ -7,11 +7,10 @@ import org.oppia.android.app.devoptions.devoptionsitemviewmodel.DeveloperOptions
 import org.oppia.android.app.devoptions.devoptionsitemviewmodel.DeveloperOptionsOverrideAppBehaviorsViewModel
 import org.oppia.android.app.devoptions.devoptionsitemviewmodel.DeveloperOptionsTestParsersViewModel
 import org.oppia.android.app.devoptions.devoptionsitemviewmodel.DeveloperOptionsViewLogsViewModel
+import org.oppia.android.app.devoptions.devoptionsitemviewmodel.DeveloperOptionsAddProfileViewModel
 import org.oppia.android.app.fragment.FragmentScope
 import org.oppia.android.domain.devoptions.ShowAllHintsAndSolutionController
 import javax.inject.Inject
-import org.oppia.android.app.devoptions.devoptionsitemviewmodel.DeveloperOptionsAddProfileViewModel
-import org.oppia.android.domain.profile.ProfileManagementController
 
 /**
  * [ViewModel] for [DeveloperOptionsFragment]. It populates the recyclerview with a list of
--- app/src/main/java/org/oppia/android/app/devoptions/devoptionsitemviewmodel/DeveloperOptionsAddProfileViewModel.kt
+++ app/src/main/java/org/oppia/android/app/devoptions/devoptionsitemviewmodel/DeveloperOptionsAddProfileViewModel.kt
@@ -1,26 +1,9 @@
 package org.oppia.android.app.devoptions.devoptionsitemviewmodel
 
-import android.content.Context
-import android.content.Intent
-import android.view.inputmethod.InputMethodManager
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContentProviderCompat.requireContext
+
 import javax.inject.Inject
-import org.oppia.android.R
 import org.oppia.android.app.devoptions.AddProfilesClickListener
-import org.oppia.android.app.devoptions.ForceCrashButtonClickListener
-import org.oppia.android.app.devoptions.RouteToMathExpressionParserTestListener
-import org.oppia.android.app.model.AddProfileActivityParams
-import org.oppia.android.app.profile.AddProfileActivity
 import org.oppia.android.app.profile.AddProfileActivityPresenter
-import org.oppia.android.app.profile.ProfileChooserActivity
-import org.oppia.android.databinding.AddProfileActivityBinding
-import org.oppia.android.domain.devoptions.ShowAllHintsAndSolutionController
-import org.oppia.android.domain.profile.ProfileManagementController
-import org.oppia.android.util.data.AsyncResult
-import org.oppia.android.util.data.DataProviders.Companion.toLiveData
-import org.oppia.android.util.extensions.getProtoExtra
 
 /**
  * [DeveloperOptionsItemViewModel] to provide features to test and debug math expressions and
