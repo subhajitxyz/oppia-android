109e3c8c94da65d0e70541b5966233b09a5e9d7d
Subhajit Mallick <153619690+subhajitxyz@users.noreply.github.com>
2025-02-04 07:25:21 +0530

    try

--- app/src/main/java/org/oppia/android/app/databinding/EditTextBindingAdapters.java
+++ app/src/main/java/org/oppia/android/app/databinding/EditTextBindingAdapters.java
@@ -13,29 +13,28 @@ import org.oppia.android.R;
 public final class EditTextBindingAdapters {
 
   /** Binding adapter for setting a [TextWatcher] as a change listener for an [EditText]. */
-  @BindingAdapter("textChangedListener")
-  public static void bindTextWatcher(@NonNull EditText editText, TextWatcher textWatcher) {
-    editText.addTextChangedListener(textWatcher);
-  }
-
 //  @BindingAdapter("textChangedListener")
 //  public static void bindTextWatcher(@NonNull EditText editText, TextWatcher textWatcher) {
-//    TextWatcher existingWatcher = (TextWatcher) editText.getTag(R.id.textWatcher);
-//    if(existingWatcher == null) {
-//      Log.d("testtextinput", "existing textwatcher is null");
-//
-//    }
-//    if (existingWatcher != null && existingWatcher != textWatcher) {
-//      editText.removeTextChangedListener(existingWatcher);
-//      Log.d("testtextinput", "Removed old TextWatcher: " + existingWatcher);
-//    }
-//
-//
-//    if (textWatcher != null) {
-//      editText.addTextChangedListener(textWatcher);
-//      editText.setTag(R.id.textWatcher, textWatcher);
-//      Log.d("testtextinput", "Added new TextWatcher: " + textWatcher);
-//    }
+//    editText.addTextChangedListener(textWatcher);
 //  }
+
+  @BindingAdapter("textChangedListener")
+  public static void bindTextWatcher(@NonNull EditText editText, TextWatcher textWatcher) {
+    TextWatcher existingWatcher = (TextWatcher) editText.getTag(R.id.textWatcher);
+    if(existingWatcher == null) {
+      Log.d("testtextinput", "existing textwatcher is null");
+
+    }
+    if (existingWatcher != null && existingWatcher != textWatcher) {
+      editText.removeTextChangedListener(existingWatcher);
+      Log.d("testtextinput", "Removed old TextWatcher: " + existingWatcher);
+    }
+
+    if (textWatcher != null) {
+      editText.addTextChangedListener(textWatcher);
+      editText.setTag(R.id.textWatcher, textWatcher);
+      Log.d("testtextinput", "Added new TextWatcher: " + textWatcher);
+    }
+  }
 
 }
