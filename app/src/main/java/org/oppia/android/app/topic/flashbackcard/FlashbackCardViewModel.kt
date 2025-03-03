8e95c36bc9a17b272408f19c957a73c942e599e7
Subhajit Mallick <153619690+subhajitxyz@users.noreply.github.com>
2025-03-03 20:18:40 +0530

    try fix 4

--- app/src/main/java/org/oppia/android/app/topic/flashbackcard/FlashbackCardViewModel.kt
+++ app/src/main/java/org/oppia/android/app/topic/flashbackcard/FlashbackCardViewModel.kt
@@ -1,6 +1,7 @@
 package org.oppia.android.app.topic.flashbackcard
 
+import javax.inject.Inject
 import org.oppia.android.app.viewmodel.ObservableViewModel
 
-class FlashbackCardViewModel: ObservableViewModel(){
+class FlashbackCardViewModel @Inject constructor(): ObservableViewModel(){
 }
