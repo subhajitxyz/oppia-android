296d7161ae516903fd39247dea382ecbb9c9091e
Subhajit Mallick <153619690+subhajitxyz@users.noreply.github.com>
2025-04-05 09:44:14 +0530

    try to use classroom activity in test 3

--- app/src/sharedTest/java/org/oppia/android/app/devoptions/DeveloperOptionsFragmentTest.kt
+++ app/src/sharedTest/java/org/oppia/android/app/devoptions/DeveloperOptionsFragmentTest.kt
@@ -709,17 +709,17 @@ class DeveloperOptionsFragmentTest {
           }
           testCoroutineDispatchers.runCurrent()
           onView(withId(R.id.home_fragment_placeholder)).check(matches(isCompletelyDisplayed()))
-          onView(withId(R.id.home_activity_drawer_layout)).perform(DrawerActions.open(GravityCompat.START))
-          //onView(withId(R.id.home_activity_drawer_layout)).check(matches(isOpen()))
+          //onView(withId(R.id.home_activity_drawer_layout)).perform(DrawerActions.open(GravityCompat.START))
+          onView(withId(R.id.home_activity_drawer_layout)).check(matches(isOpen()))
 
 
-          onView(withText(R.string.menu_switch_profile)).perform(click())
-          onView(withText(R.string.home_activity_back_dialog_message))
-            .inRoot(RootMatchers.isDialog())
-            .check(matches(isDisplayed()))
+//          onView(withText(R.string.menu_switch_profile)).perform(click())
+//          onView(withText(R.string.home_activity_back_dialog_message))
+//            .inRoot(RootMatchers.isDialog())
+//            .check(matches(isDisplayed()))
 
           onView(withId(R.id.drawer_nested_scroll_view)).perform(ViewActions.swipeUp())
-          onView(withId(R.id.administrator_controls_linear_layout)).check(matches(isDisplayed()))
+          //onView(withId(R.id.administrator_controls_linear_layout)).check(matches(isDisplayed()))
 
           // Verify developer options layout is displayed
           onView(withId(R.id.developer_options_linear_layout)).check(matches(isDisplayed()))
