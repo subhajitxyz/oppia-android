c8711c9a9a44d14477cc213ee5cca43db5ecb39d
Subhajit Mallick <153619690+subhajitxyz@users.noreply.github.com>
2025-04-10 11:17:20 +0530

    add test in statefragmenttest

--- app/src/sharedTest/java/org/oppia/android/app/player/state/StateFragmentTest.kt
+++ app/src/sharedTest/java/org/oppia/android/app/player/state/StateFragmentTest.kt
@@ -2118,6 +2118,43 @@ class StateFragmentTest {
     }
   }
 
+  //subha
+  @Test
+  fun testStateFragment_landscape_forMisconception_clickLinkText_opensConceptCard_closeConceptCard() {
+    setUpTestWithLanguageSwitchingFeatureOff()
+    launchForExploration(FRACTIONS_EXPLORATION_ID_1, shouldSavePartialProgress = false).use {
+      rotateToLandscape()
+      startPlayingExploration()
+      selectMultipleChoiceOption(
+        optionPosition = 3,
+        expectedOptionText = "No, because, in a fraction, the pieces must be the same size."
+      )
+      clickSubmitAnswerButton()
+      clickContinueNavigationButton()
+      typeFractionText("3/2") // Misconception.
+      clickSubmitAnswerButton()
+
+      onView(withId(R.id.feedback_text_view)).perform(openClickableSpan("refresher lesson"))
+      testCoroutineDispatchers.runCurrent()
+
+      onView(withText("Concept Card")).inRoot(isDialog()).check(matches(isDisplayed()))
+      onView(withId(R.id.concept_card_heading_text))
+        .inRoot(isDialog())
+        .check(matches(withText(containsString("Identify the numerator and denominator"))))
+
+
+      //try to close
+      onView(withContentDescription(R.string.navigate_up)).perform(click())
+
+      testCoroutineDispatchers.runCurrent()
+      onView(withId(R.id.concept_card_toolbar)).check(doesNotExist())
+      onView(withText("Concept Card"))
+        .inRoot(isDialog())
+        .check(doesNotExist())
+
+    }
+  }
+
   @Test
   fun testStateFragment_interactions_initialStateIsContinueInteraction() {
     setUpTestWithLanguageSwitchingFeatureOff()
