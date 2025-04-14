c55ca6ca69ee3f84638e76845f9e1d603963d92c
Subhajit Mallick <153619690+subhajitxyz@users.noreply.github.com>
2025-04-15 01:55:45 +0530

    add test in developeroptionsactivitytes 15

--- app/src/sharedTest/java/org/oppia/android/app/devoptions/DeveloperOptionsFragmentTest.kt
+++ app/src/sharedTest/java/org/oppia/android/app/devoptions/DeveloperOptionsFragmentTest.kt
@@ -694,31 +694,33 @@ class DeveloperOptionsFragmentTest {
       scrollToPosition(position = 4)
       onView(withId(R.id.add_three_profiles_text_view)).perform(click())
       testCoroutineDispatchers.runCurrent()
-//      intended(hasComponent(ProfileChooserActivity::class.java.name))
-//
-//      launch(ProfileChooserActivity::class.java).use {
-//        testCoroutineDispatchers.runCurrent()
-//
-//        onView(withId(R.id.profile_recycler_view)).check(matches(isDisplayed()))
-//        onView(withId(R.id.profile_recycler_view)).check(hasItemCount(count = 5))
-//
-//        onView(withId(R.id.profile_recycler_view)).perform(scrollToPosition<ViewHolder>(0))
-//        verifyTextOnProfileListItemAtPosition(
-//          itemPosition = 0,
-//          targetView = R.id.profile_name_text,
-//          stringToMatch = "Admin"
-//        )
-//
-//        onView(withId(R.id.profile_recycler_view)).perform(scrollToPosition<ViewHolder>(4))
-//        verifyTextOnProfileListItemAtPosition(
-//          itemPosition = 4,
-//          targetView = R.id.add_profile_text,
-//          stringToMatch = context.getString(R.string.profile_chooser_add)
-//        )
-//      }
+      intended(hasComponent(ProfileChooserActivity::class.java.name))
+
+      launch(ProfileChooserActivity::class.java).use {
+        testCoroutineDispatchers.runCurrent()
+
+        onView(withId(R.id.profile_recycler_view)).check(matches(isDisplayed()))
+        onView(withId(R.id.profile_recycler_view)).check(hasItemCount(count = 5))
+
+        onView(withId(R.id.profile_recycler_view)).perform(scrollToPosition<ViewHolder>(0))
+        verifyTextOnProfileListItemAtPosition(
+          itemPosition = 0,
+          targetView = R.id.profile_name_text,
+          stringToMatch = "Admin"
+        )
+
+        onView(withId(R.id.profile_recycler_view)).perform(scrollToPosition<ViewHolder>(4))
+        verifyTextOnProfileListItemAtPosition(
+          itemPosition = 4,
+          targetView = R.id.add_profile_text,
+          stringToMatch = context.getString(R.string.profile_chooser_add)
+        )
+      }
 
       onView(withId(R.id.show_profile_count))
         .check(matches(ViewMatchers.withText("4")))
+      onView(withId(R.id.profile_recycler_view)).check(hasItemCount(count = 5))
+
     }
   }
 
