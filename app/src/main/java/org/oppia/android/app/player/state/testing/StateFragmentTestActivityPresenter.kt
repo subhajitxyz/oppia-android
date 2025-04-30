f796ce98aece2f8395665290d3cee926dd22659c
Subhajit Mallick <153619690+subhajitxyz@users.noreply.github.com>
2025-04-30 23:05:34 +0530

    add test7

--- app/src/main/java/org/oppia/android/app/player/state/testing/StateFragmentTestActivityPresenter.kt
+++ app/src/main/java/org/oppia/android/app/player/state/testing/StateFragmentTestActivityPresenter.kt
@@ -23,7 +23,6 @@ import org.oppia.android.util.data.DataProviders.Companion.toLiveData
 import org.oppia.android.util.extensions.getProtoExtra
 import javax.inject.Inject
 import org.oppia.android.app.player.exploration.ExplorationFragment
-import org.oppia.android.app.player.exploration.TAG_EXPLORATION_FRAGMENT
 
 private const val TEST_ACTIVITY_TAG = "TestActivity"
 private const val TAG_EXPLORATION_FRAGMENT = "TAG_EXPLORATION_FRAGMENT"
