e43cff518a4ef79f545bd2b8198500dde6cf4196
Subhajit Mallick <153619690+subhajitxyz@users.noreply.github.com>
2025-05-07 01:05:52 +0530

    add test for profilecount 20

--- app/src/sharedTest/java/org/oppia/android/app/devoptions/DeveloperOptionsFragmentTest.kt
+++ app/src/sharedTest/java/org/oppia/android/app/devoptions/DeveloperOptionsFragmentTest.kt
@@ -700,38 +700,6 @@ class DeveloperOptionsFragmentTest {
       .createProfileChooserActivity(ApplicationProvider.getApplicationContext())//ApplicationProvider.getApplicationContext()
   }
 
-  @Test
-  fun testDeveloperOptions_clickAddThreeProfiles_checksThreeProfilesAreAdded_Chatgpt() {
-   // Intents.init() // If not already handled by @RunOn
-    launch<DeveloperOptionsTestActivity>(
-      createDeveloperOptionsTestActivityIntent(internalProfileId)
-    ).use {
-      testCoroutineDispatchers.runCurrent()
-      scrollToPosition(position = 4)
-      onView(withId(R.id.add_three_profiles_text_view)).perform(click())
-      testCoroutineDispatchers.runCurrent()
-
-      // At this point, ProfileChooserActivity is expected to be launched by an intent
-      intended(hasComponent(ProfileChooserActivity::class.java.name))
-
-      // Continue by interacting with the UI that results from that intent
-      onView(withId(R.id.profile_recycler_view)).check(matches(isDisplayed()))
-      onView(withId(R.id.profile_recycler_view)).check(RecyclerViewMatcher.hasItemCount(count = 5))
-
-      onView(
-        atPositionOnView(
-          recyclerViewId = R.id.profile_recycler_view,
-          position = 0,
-          targetViewId = R.id.profile_chooser_item
-        )
-      ).perform(click())
-
-      // This is the actual intent triggered by that click
-      intended(hasComponent(HomeActivity::class.java.name))
-      intended(IntentMatchers.hasExtraWithKey(PROFILE_ID_INTENT_DECORATOR))
-    }
-   // Intents.release()
-  }
 
 
   @Test
@@ -747,8 +715,11 @@ class DeveloperOptionsFragmentTest {
 
       intended(hasComponent(ProfileChooserActivity::class.java.name))
 
+      val actualIntent = Intents.getIntents()
+        .first { it.component?.className == ProfileChooserActivity::class.java.name }
+
       //launch(ProfileChooserActivity::class.java).use {
-      launch<ProfileChooserActivity>(createProfileChooserActivityIntent()).use {
+      launch<ProfileChooserActivity>(actualIntent).use {
         testCoroutineDispatchers.runCurrent()
 
         onView(withId(R.id.profile_recycler_view)).check(matches(isDisplayed()))
@@ -771,12 +742,14 @@ class DeveloperOptionsFragmentTest {
     }
 
 
-    intended(hasComponent(ProfileChooserActivity::class.java.name))
 
     intended(hasComponent(HomeActivity::class.java.name))
 
+    val actualIntenthome = Intents.getIntents()
+      .first { it.component?.className == HomeActivity::class.java.name }
+
     val homeScenario =
-      ActivityScenario.launch<HomeActivity>(createHomeActivityIntent(internalProfileId))
+      ActivityScenario.launch<HomeActivity>(actualIntenthome)
 
     homeScenario.use {
       onView(withContentDescription(R.string.drawer_open_content_description))
