573bfb6eaf24443ab02e2706d654e73975b1e36f
Subhajit Mallick <153619690+subhajitxyz@users.noreply.github.com>
2025-05-06 22:44:55 +0530

    add test for profilecount 4

--- app/src/sharedTest/java/org/oppia/android/app/devoptions/DeveloperOptionsFragmentTest.kt
+++ app/src/sharedTest/java/org/oppia/android/app/devoptions/DeveloperOptionsFragmentTest.kt
@@ -730,59 +730,51 @@ class DeveloperOptionsFragmentTest {
         onView(withId(R.id.profile_recycler_view))
           .perform(scrollToPosition<ViewHolder>(0))
           .perform(click())
+
+        testCoroutineDispatchers.runCurrent()
+
+
       }
-        testCoroutineDispatchers.runCurrent()
-        //intended(hasComponent(HomeActivity::class.java.name))
+    }
 
+    intended(hasComponent(HomeActivity::class.java.name))
 
-      launch(HomeActivity::class.java).use { homeScenario ->
+    launch(HomeActivity::class.java).use { homeScenario ->
 
-        onView(withContentDescription(R.string.drawer_open_content_description))
-          .check(matches(isCompletelyDisplayed()))
-          .perform(click())
-        testCoroutineDispatchers.runCurrent()
+      onView(withContentDescription(R.string.drawer_open_content_description))
+        .check(matches(isCompletelyDisplayed()))
+        .perform(click())
+      testCoroutineDispatchers.runCurrent()
 
-        // Open the navigation drawer
-        homeScenario.onActivity { activity ->
-          val drawerLayout = activity.findViewById<DrawerLayout>(R.id.home_activity_drawer_layout)
-          drawerLayout.computeScroll()
+      // Open the navigation drawer
+      homeScenario.onActivity { activity ->
+        val drawerLayout = activity.findViewById<DrawerLayout>(R.id.home_activity_drawer_layout)
+        drawerLayout.computeScroll()
 
-          //testing
-          onView(withId(R.id.home_activity_drawer_layout)).check(matches(DrawerMatchers.isOpen()))
-
-          onView(withId(R.id.drawer_nested_scroll_view)).perform(ViewActions.swipeUp())
-          //there can be problem to show developer options
-          onView(withId(R.id.developer_options_linear_layout)).check(matches(isDisplayed()))
-
-          // Click developer options
-          onView(withId(R.id.developer_options_linear_layout)).perform(click())
-        }
-        testCoroutineDispatchers.runCurrent()
-        onView(withId(R.id.home_activity_drawer_layout)).check(matches(DrawerMatchers.isOpen()))
-
-        onView(withId(R.id.drawer_nested_scroll_view)).perform(ViewActions.swipeUp())
-        //there can be problem to show developer options
-        onView(withId(R.id.developer_options_linear_layout)).check(matches(isDisplayed()))
-
-        // Click developer options
-        onView(withId(R.id.developer_options_linear_layout)).perform(click())
-
-        testCoroutineDispatchers.runCurrent()
-        intended(hasComponent(DeveloperOptionsActivity::class.java.name))
-
-        launch(DeveloperOptionsActivity::class.java).use {
-          testCoroutineDispatchers.runCurrent()
-          scrollToPosition(position = 4)
-
-          onView(withId(R.id.existing_profile_count_text_view))
-            .check(matches(ViewMatchers.withText("Existing Profile Count")))
-
-          onView(withId(R.id.show_profile_count))
-            .check(matches(ViewMatchers.withText("4")))
-        }
       }
+      testCoroutineDispatchers.runCurrent()
+      onView(withId(R.id.home_activity_drawer_layout)).check(matches(DrawerMatchers.isOpen()))
 
+      onView(withId(R.id.drawer_nested_scroll_view)).perform(ViewActions.swipeUp())
+      //there can be problem to show developer options
+      onView(withId(R.id.developer_options_linear_layout)).check(matches(isDisplayed()))
 
+      // Click developer options
+      onView(withId(R.id.developer_options_linear_layout)).perform(click())
+
+      testCoroutineDispatchers.runCurrent()
+      intended(hasComponent(DeveloperOptionsActivity::class.java.name))
+
+      launch(DeveloperOptionsActivity::class.java).use {
+        testCoroutineDispatchers.runCurrent()
+        scrollToPosition(position = 4)
+
+        onView(withId(R.id.existing_profile_count_text_view))
+          .check(matches(ViewMatchers.withText("Existing Profile Count")))
+
+        onView(withId(R.id.show_profile_count))
+          .check(matches(ViewMatchers.withText("4")))
+      }
     }
   }
 
